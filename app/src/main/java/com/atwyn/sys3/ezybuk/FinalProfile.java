ecf95a4c0b3eb3a8c645246b17c948389527463e
SYS3 <khushbunahar@gmail.com>
2017-08-24 17:33:11 +0530

    24-8

--- app/src/main/java/com/atwyn/sys3/ezybuk/FinalProfile.java
+++ app/src/main/java/com/atwyn/sys3/ezybuk/FinalProfile.java
@@ -2,9 +2,16 @@ package com.atwyn.sys3.ezybuk;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -13,16 +20,68 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 public class FinalProfile extends AppCompatActivity {
     EditText ed1, ed2, ed3, ed4;
+    //  JSONParser jsonParser = new JSONParser();
     String finalprofilname, finalprofileemail, finalprofilemobileno;
     private DatePicker datePicker;
     private Calendar calendar;
-
+    private boolean loggedIn = false;
     private int year, month, day;
-    Button save;
+    Button save, selectdate;
+    String gender = "";
+    private Button pPickDate;
+    private int pYear;
+    private int pMonth;
+    private int pDay;
+    private static final String REGISTER_URL = Config.EzyBuk_SignUp;
+    private static final String TAG_SUCCESS = "success";
+    /**
+     * This integer will uniquely define the dialog to be used for displaying date picker.
+     */
+    static final int DATE_DIALOG_ID = 0;
+    private DatePickerDialog.OnDateSetListener pDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+
+                public void onDateSet(DatePicker view, int year,
+                                      int monthOfYear, int dayOfMonth) {
+                    pYear = year;
+                    pMonth = monthOfYear;
+                    pDay = dayOfMonth;
+                    updateDisplay();
+                    displayToast();
+                }
+            };
+
+    /**
+     * Updates the date in the TextView
+     */
+    private void updateDisplay() {
+        ed3.setText(
+                new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(pMonth + 1).append("/")
+                        .append(pDay).append("/")
+                        .append(pYear).append(" "));
+
+    }
+
+    /**
+     * Displays a notification when the date is updated
+     */
+    private void displayToast() {
+        Toast.makeText(this, new StringBuilder().append("Date choosen is ").append(ed3.getText()), Toast.LENGTH_SHORT).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,57 +97,231 @@ public class FinalProfile extends AppCompatActivity {
         ed2 = (EditText) findViewById(R.id.editText_user2);
         ed3 = (EditText) findViewById(R.id.editText_user22);
         ed4 = (EditText) findViewById(R.id.editText_user222);
+        selectdate = (Button) findViewById(R.id.button8);
         save = (Button) findViewById(R.id.savefinal);
         final RadioGroup rgroup = (RadioGroup) findViewById(R.id.radioGender);
         final RadioButton male = (RadioButton) findViewById(R.id.genderMale);
         final RadioButton female = (RadioButton) findViewById(R.id.genderFemale);
 
+
         ed1.setText(finalprofilname);
         ed2.setText(finalprofilemobileno);
         ed4.setText(finalprofileemail);
-    }
-}
+        selectdate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
 
-       /* calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
 
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month + 1, day);
+        final Calendar cal = Calendar.getInstance();
+        pYear = cal.get(Calendar.YEAR);
+        pMonth = cal.get(Calendar.MONTH);
+        pDay = cal.get(Calendar.DAY_OF_MONTH);
+
+
+        updateDisplay();
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = ed1.getText().toString();
+
+                String email = ed4.getText().toString();
+                String mobileno = ed2.getText().toString();
+                String dob = ed3.getText().toString();
+
+                if (rgroup.getCheckedRadioButtonId() == male.getId()) {
+                    gender = "Male";
+                } else if (rgroup.getCheckedRadioButtonId() == female.getId()) {
+                    gender = "Female";
+                }
+
+                if (name.contentEquals("") || email.contentEquals("") || mobileno.contentEquals("") || dob.contentEquals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(FinalProfile.this);
+
+                    builder.setMessage("Fill Data")
+                            .setTitle(" Data");
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+
+                    AlertDialog dialog = builder.show();
+                } else {
+                 registerUser();
+                }
+            }
+        });
     }
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-        Toast.makeText(getApplicationContext(), "ca",
-                Toast.LENGTH_SHORT)
-                .show();
+
+
+
+//    class RegisterNewUser extends AsyncTask<String, String, String> {
+//        protected String doInBackground(String... args) {
+//            String name1 = ed1.getText().toString();
+//            String mobileno1 = ed2.getText().toString();
+//            String email1 = ed4.getText().toString();
+//            String dob1 = ed3.getText().toString();
+//
+//            List<NameValuePair> params = new ArrayList<NameValuePair>();
+//            params.add(new BasicNameValuePair("name", name1));
+//            params.add(new BasicNameValuePair("username", mobileno1));
+//            params.add(new BasicNameValuePair("email", email1));
+//            params.add(new BasicNameValuePair("password", dob1));
+//            params.add(new BasicNameValuePair("role", gender));
+//
+//            JSONObject json = jsonParser.makeHttpRequest(Config.EzyBuk_SignUp,
+//                    "GET", params);
+//
+//            Log.d("Send Notification", json.toString());
+//
+//            try
+//            {
+//                int success = json.getInt(TAG_SUCCESS);
+//
+//                if (success == 1)
+//                {
+//
+//                  /*  if   (gender.contentEquals("Student"))
+//                    {
+//                        Intent i = new Intent(getApplicationContext(), StudMain.class);
+//                        startActivity(i);
+//                        finish();
+//                    }
+//                    else if ((role.contentEquals("Shop Owner")))
+//                    {
+//                        Intent i = new Intent(getApplicationContext(), ShopMain.class);
+//                        startActivity(i);
+//                        finish();
+//                    }
+//                    else
+//                    {
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+//
+//                        builder.setMessage(R.string.roleNullAlert)
+//                                .setTitle(R.string.alertTitle);
+//
+//                        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//
+//                            }
+//                        });
+//
+//                        AlertDialog dialog = builder.show();
+//                    }
+//
+//                }
+//
+//                else
+//                {
+//
+//                }*/
+//            }
+//
+//            catch (Exception e)
+//            {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//    }
+
+    private void registerUser() {
+      /*  editTextConfirmPassword.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                String strPass1 = editTextPassword.getText().toString();
+                String strPass2 = editTextConfirmPassword.getText().toString();
+                if (strPass1.equals(strPass2)) {
+                    Toast.makeText(getApplicationContext(), "Password Equal", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Password Not Equal", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });*/
+
+
+//            String mobileno1 = ed2.getText().toString();
+//            String email1 = ed4.getText().toString();
+//            String dob1 = ed3.getText().toString();
+        String name1 = ed1.getText().toString().trim().toLowerCase();
+        String mobileno1 = ed2.getText().toString().trim().toLowerCase();
+        String email1 = ed4.getText().toString().trim().toLowerCase();
+        String dob1 = ed3.getText().toString().trim().toLowerCase();
+String gend=gender;
+        register(name1,mobileno1,email1,dob1,gend);
     }
+
+    private void register(String name1, final String mobileno1, String email1, final String dob1, final String gend) {
+        class RegisterUser extends AsyncTask<String, Void, String>{
+            ProgressDialog loading;
+            RegisterUserClass ruc = new RegisterUserClass();
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(FinalProfile.this, "Please Wait",null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+             /*   Intent in=new Intent(FinalProfile.this, PaymentOptions.class);
+                in.putExtra("EzyBuk_name",name);
+                in.putExtra("EzyBuk_Email",email);
+                in.putExtra("EzyBuk_mobile",mobile);
+                startActivity(in);
+                Toast.makeText(getApplicationContext(),"Successfully Registered..."+s,Toast.LENGTH_LONG).show();*/
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String,String>();
+                data.put("name",params[0]);
+                data.put("email",params[1]);
+                data.put("mobileno",params[2]);
+                data.put("dob",params[3]);
+                data.put("gender",params[4]);
+
+                String result = ruc.sendPostRequest(REGISTER_URL,data);
+
+                return  result;
+
+            }
+        }
+
+        RegisterUser ru = new RegisterUser();
+        ru.execute(name1,mobileno1,email1,dob1,gend);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(this,
-                    myDateListener, year, month, day);
+protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case DATE_DIALOG_ID:
+        return new DatePickerDialog(this,
+        pDateSetListener,
+        pYear, pMonth, pDay);
         }
         return null;
-    }
-
-    private DatePickerDialog.OnDateSetListener myDateListener = new
-            DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker arg0,
-                                      int arg1, int arg2, int arg3) {
-                    // TODO Auto-generated method stub
-                    // arg1 = year
-                    // arg2 = month
-                    // arg3 = day
-                    showDate(arg1, arg2+1, arg3);
-                }
-            };
-
-    private void showDate(int year, int month, int day) {
-        ed3.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-    }
-}*/
+        }
+        }
