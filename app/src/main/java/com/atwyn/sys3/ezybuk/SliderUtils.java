6489918365d1ab953d90f428d422503d37663d44
SYS3 <khushbunahar@gmail.com>
2017-09-11 15:51:18 +0530

    changes 11-9-2017

---
+++ app/src/main/java/com/atwyn/sys3/ezybuk/SliderUtils.java
@@ -0,0 +1,18 @@
+package com.atwyn.sys3.ezybuk;
+
+/**
+ * Created by SYS3 on 9/7/2017.
+ */
+
+public class SliderUtils {
+
+    String sliderImageUrl;
+
+    public String getSliderImageUrl() {
+        return sliderImageUrl;
+    }
+
+    public void setSliderImageUrl(String sliderImageUrl) {
+        this.sliderImageUrl = sliderImageUrl;
+    }
+}
