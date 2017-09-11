62966343f871fdbe034d3eac43be8baf1802fb40
SYS3 <khushbunahar@gmail.com>
2017-09-11 15:50:37 +0530

    changes 11-9-2017

---
+++ app/src/main/java/com/atwyn/sys3/ezybuk/MyRecyclerViewAdapter.java
@@ -0,0 +1,85 @@
+package com.atwyn.sys3.ezybuk;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by SYS3 on 9/4/2017.
+ */
+
+class MyRecyclerViewAdapter extends RecyclerView
+        .Adapter<MyRecyclerViewAdapter
+        .DataObjectHolder> {
+    private static String LOG_TAG = "MyRecyclerViewAdapter";
+    private ArrayList<MySQLDataBase> mDataset;
+    private static MyClickListener myClickListener;
+
+    public static class DataObjectHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+        TextView label;
+        TextView dateTime;
+
+        public DataObjectHolder(View itemView) {
+            super(itemView);
+            label = (TextView) itemView.findViewById(R.id.theatername12);
+            dateTime = (TextView) itemView.findViewById(R.id.buttontime);
+            Log.i(LOG_TAG, "Adding Listener");
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            myClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(MyClickListener myClickListener) {
+        this.myClickListener = myClickListener;
+    }
+
+    public MyRecyclerViewAdapter(ArrayList<MySQLDataBase> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cardviewseatselection, parent, false);
+
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position) {
+        holder.label.setText(mDataset.get(position).getTitle());
+        holder.dateTime.setText(mDataset.get(position).getShowTime());
+    }
+
+    public void addItem(MySQLDataBase dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        public void onItemClick(int position, View v);
+    }
+}
